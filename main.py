b1522a5e42aee9387c4dc315f6cdeb5674da24d4
bzantium <ryumin93@gmail.com>
2021-06-02 10:18:34 +0900

    dump

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def hello_world(name):
     msg = f"Hello World!, {name}"
-    print(msg)
+    print(msg)  # hmm
     return msg
 
 
 def hello_world(name):
     msg = f"Hello World!, {name}"
-    print(msg)
+    print(msg)  # hmm
     return msg
-    
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # pragma: no cover
     hello_world("Minho")
